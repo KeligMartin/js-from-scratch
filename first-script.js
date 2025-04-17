0cdaba846f9853931203f969f06686f325d2a741
kmartin <kmartin@ipsosenso.com>
2025-04-17 10:58:10 +0200

    arrays

--- first-script.js
+++ first-script.js
@@ -1,15 +1,17 @@
-for(let i = 0; i < 100; i++) {
-	isPrime(i);
+class Task {
+	constructor(title, description, priority) {
+		this.title = title
+		this.description = description
+		this.priority = priority
+	}
 }
 
-function isPrime(monNombre) {
-	let count = 0;
-	for (let j = monNombre; j > 0; j--) {
-		if( monNombre % j === 0) {
-			count++
-		}
-	}
-	if(count === 2) {
-		console.log(monNombre)
-	}
-}
+let task1 = new Task("Créer le HTMl", "Structurer le site", 1)
+let task2 = new Task("Créer le CSS", "Styliser le site", 5)
+let task3 = new Task("Créer le JS", "Animer le site", 3)
+let task4 = new Task("Créer le php", "Créer le backend", 19)
+let task5 = new Task("Créer la BDD", "instancier la BDD", 100)
+
+const tasks = [task1, task2, task3, task4, task5]
+
+console.table(tasks.sort((a, b) => b.priority - a.priority))
