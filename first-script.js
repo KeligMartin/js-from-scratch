9ce3ca72515274f0930a350d203b1c1a6322c27d
kmartin <kmartin@ipsosenso.com>
2025-05-13 15:26:47 +0200

    exo 2

--- first-script.js
+++ first-script.js
@@ -1,22 +1,41 @@
 class Task {
-	constructor(title, description, priority) {
+	constructor(title, description, priority, timeSpent, owner) {
 		this.title = title
 		this.description = description
 		this.priority = priority
+		this.timeSpent = timeSpent
+		this.owner = `${owner.name} ${owner.firstname}`
 	}
 }
 
-let task1 = new Task("Créer le HTMl", "Structurer le site", 1)
-let task2 = new Task("Créer le CSS", "Styliser le site", 5)
-let task3 = new Task("Créer le JS", "Animer le site", 3)
-let task4 = new Task("Créer le php", "Créer le backend", 19)
-let task5 = new Task("Créer la BDD", "instancier la BDD", 100)
+class Owner {
+	constructor(name, firstname) {
+		this.name = name;
+		this.firstname = firstname
+	}
+}
+
+const owner = new Owner('Martin', 'Kelig')
+
+let task1 = new Task("Créer le HTMl", "Structurer le site", 1, 10, owner)
+let task2 = new Task("Créer le CSS", "Styliser le site", 5, 10,  owner)
+let task3 = new Task("Créer le JS", "Animer le site", 3, 10,  owner)
+let task4 = new Task("Créer le php", "Créer le backend", 19, 10, owner)
+let task5 = new Task("Créer la BDD", "instancier la BDD", 100, 10, owner)
 
 const tasks = [task1, task2, task3, task4, task5]
 
 tasks.forEach((task, index) => {
-	console.log(task, index)
+	if(index % 2 !== 0) {
+		task.title = task.title.toUpperCase()
+	}
 })
+
+const falseTask = new Task('Total temps passé', '', 0, tasks.reduce((acc, curr) => acc + curr.timeSpent, 0), owner)
+tasks.push(falseTask)
+
+console.table(tasks)
+
 
 // natif
 // attention à la condition
@@ -40,14 +59,9 @@ for(let i = 0; i < tasks.length; i++) {
 }
 
 
-const tasksMap = tasks.map((task) => new Task(task.title, task.description, task.priority * 2))
+const tasksMap = tasks.map((task) => new Task(task.title, task.description, task.priority * 2, 0, owner))
 
 // natif
-
-const tasksMapNatif = []
-for(let i = 0; i < tasks.length; i++) {
-	tasksMapNatif.push(new Task(tasks[i].title, tasks[i].description, tasks[i].priority));
-}
 
 const priorityTotal = tasks.reduce((acc, currentValue) => acc + currentValue.priority, 0)
 
